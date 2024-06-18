59ab1b2cd4daef8678ac43932f19529a15b90cf0
viktor <pistoletviktor@gmal.com>
2024-06-18 18:08:03 +0300

    Gateway-api module is successfully added

---
+++ gateway-api/src/test/java/ru/kashtanov/gateway_api/GatewayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kashtanov.gateway_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
